f1bcee253d9551f66f14087e49cbe85c3d292eab
dmitriy.turovtsov <web.best.master@gmail.com>
2016-10-13 16:04:14 +0300

    add my gulp util

--- spree-shop-1/gulpfile.js
+++ spree-shop-1/gulpfile.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const path = require('path');
 
 const gulp = require('gulp');
@@ -17,6 +16,8 @@ const rootFolder = 'www';
 const rootStaticFolder = path.join(rootFolder, 'static');
 const distFolder = 'dist';
 const distStaticFolder = path.join(distFolder, 'static');
+
+const readFiles = require('./my-gulp-util').readFiles;
 
 gulp.task('clean', function () {
     return gulp.src(path.join(distFolder, '**', '*'), { read: false })
@@ -105,73 +106,3 @@ gulp.task('watch:copy-data', function () {
 
 gulp.task('default', gulp.series('clean', gulp.parallel('html', 'css', 'js', 'copy-data')));
 gulp.task('watch', gulp.series('default', gulp.parallel('watch:html', 'watch:css', 'watch:js', 'watch:copy-data')) );
-
-
-
-
-// util section
-function _walk(dir, done) {
-    var results = [];
-    fs.readdir(dir, function (err, list) {
-        if (err) return done(err);
-        var pending = list.length;
-        if (!pending) return done(null, results);
-        list.forEach(function (file) {
-            file = path.resolve(dir, file);
-            fs.stat(file, function (err, stat) {
-                if (stat && stat.isDirectory()) {
-                    _walk(file, function (err, res) {
-                        results = results.concat(res);
-                        if (!--pending) done(null, results);
-                    });
-                } else {
-                    results.push(file);
-                    if (!--pending) done(null, results);
-                }
-            });
-        });
-    });
-}
-
-function walk(path) {
-    return new Promise((resolve, reject) =>
-        _walk(path, (err, result) =>
-            err ? reject(err) : resolve(result)
-        )
-    );
-}
-
-function readFile(path) {
-
-    return new Promise((resolve, reject) =>
-        fs.readFile(path, 'utf8', (err, data) =>
-            err ? reject(err) : resolve({path, data})
-        )
-    );
-}
-
-function readFiles(pathToFolder, {test = /[\s\S]+/}) {
-
-    return walk(pathToFolder)
-        .then(paths =>
-            Promise.all(paths
-                .filter(() => test.test)
-                .map(readFile)
-            )
-        )
-        .then(files => {
-                var map = {};
-                files.forEach(
-                    file => {
-                        var key = file.path
-                            .replace(path.resolve(__dirname, pathToFolder), '')
-                            .replace(path.sep, '');
-                        map[key] = file.data;
-                    }
-                );
-                return map;
-            }
-        );
-
-}
-
---
+++ spree-shop-1/my-gulp-util.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+
+// util section
+function _walk(dir, done) {
+    var results = [];
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(function (file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    _walk(file, function (err, res) {
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.push(file);
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+}
+
+function walk(path) {
+    return new Promise((resolve, reject) =>
+        _walk(path, (err, result) =>
+            err ? reject(err) : resolve(result)
+        )
+    );
+}
+
+function readFile(path) {
+
+    return new Promise((resolve, reject) =>
+        fs.readFile(path, 'utf8', (err, data) =>
+            err ? reject(err) : resolve({path, data})
+        )
+    );
+}
+
+module.exports.readFiles = function readFiles(pathToFolder, {test = /[\s\S]+/}) {
+
+    return walk(pathToFolder)
+        .then(paths =>
+            Promise.all(paths
+                .filter(() => test.test)
+                .map(readFile)
+            )
+        )
+        .then(files => {
+                var map = {};
+                files.forEach(
+                    file => {
+                        var key = file.path
+                            .replace(path.resolve(__dirname, pathToFolder), '')
+                            .replace(path.sep, '');
+                        map[key] = file.data;
+                    }
+                );
+                return map;
+            }
+        );
+
+};
